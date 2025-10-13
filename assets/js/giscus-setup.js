7838aafa0bc78967a7c408cf21ce0c4eb3dda1e9
leilacstl <leila.costil@gmail.com>
2025-10-13 14:05:13 +0000

    Deploying to gh-pages from @ leilacstl/leilacstl.github.io@5a9fa9921f847698959b8e6d607c9458415f51a4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "leilacstl/portfolio",
+    "data-repo": "leilacstl/leilacstl.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
